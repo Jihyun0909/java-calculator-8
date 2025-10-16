81c9694a8526b6111dff747c700a15ce12cd5873
Jihyun0909 <quswlgus0909@naver.com>
2025-10-16 18:07:58 +0900

    feat(app): Controller에서 서비스 호출로 계산 흐름 연결

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,13 +1,14 @@
 package calculator.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import calculator.service.StringAddCalculator;
 
 public class CalculatorController {
 
     public void run() {
         String input = readInput();
-        // 다음 단계에서 StringAddCalculator.add(input) 호출로 교체
-        printResult(0); // 임시 출력
+        int result = StringAddCalculator.add(input);
+        printResult(result);
     }
 
     private String readInput() {
